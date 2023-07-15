dfb708b10b15b507ed3e456bcf7ef505ef00cf0a
Robert Okuni <o.robert1994@htomail.com>
2023-07-16 01:15:21 +0200

    newPages

--- pages/welcomePage.js
+++ pages/welcomePage.js
@@ -2,80 +2,88 @@ function initializeWelcomePage() {
   const welcomeContainer = document.getElementById('welcome-container');
   const loadingContainer = document.getElementById('loading-container');
   const searchContainer = document.querySelector('.search-container');
-  // the welcome header
+
+  // Create the welcome header
   const welcomeHeader = document.createElement('div');
   welcomeHeader.classList.add('welcomeHeader');
+
   const welcomeHeading = document.createElement('h1');
   welcomeHeading.classList.add('fade-in');
-  setTimeout(() => {
-    welcomeHeading.textContent = 'EASY WAY TO ENJOY YOUR FAVOURITE MUSIC';
-  }, 2000);
+  welcomeHeading.textContent = 'EASY WAY TO ENJOY YOUR FAVOURITE MUSIC';
 
   welcomeHeader.appendChild(welcomeHeading);
 
-  // the small header
+  // Create the small header
   const smallHeader = document.createElement('div');
-  const smallHeading = document.createElement('h2');
   smallHeader.classList.add('smallHeader');
+
+  const smallHeading = document.createElement('h2');
   smallHeading.textContent = 'SEARCH PLAY FOR FREE';
 
   smallHeader.appendChild(smallHeading);
 
-  //the phone image container
+  // Create the phone image container
   const phoneImgContainer = document.createElement('div');
   phoneImgContainer.classList.add('phoneImgContainer');
 
-  // a div to hold search and search button
+  // Create a div to hold search and search button
   const itemDiv = document.createElement('div');
   itemDiv.classList.add('phoneSrchComponents');
   phoneImgContainer.appendChild(itemDiv);
 
-  //  the search songs element
+  // Create the search songs element
   const searchSongs = document.createElement('div');
   searchSongs.classList.add('srchCont');
+
   const searchSongsHeading = document.createElement('h3');
   searchSongsHeading.textContent = 'search songs';
+
   searchSongs.appendChild(searchSongsHeading);
 
-  // the search button element
+  // Create the search button element
   const searchButton = document.createElement('div');
   searchButton.classList.add('srchBtn');
+
   const searchButtonHeading = document.createElement('h3');
   searchButtonHeading.textContent = 'search';
+
   searchButton.appendChild(searchButtonHeading);
 
-  // the search results element
+  // Create the search results element
   const searchResults = document.createElement('div');
   searchResults.classList.add('resultsCont');
+
   const searchResultsHeading = document.createElement('h3');
   searchResultsHeading.textContent = 'Search Results';
+
   searchResults.appendChild(searchResultsHeading);
 
   // Create the start button container
   const startButtonContainer = document.createElement('div');
   startButtonContainer.classList.add('btn');
+
   const startButton = document.createElement('button');
   startButton.id = 'start-button';
-  setTimeout(() => {
-    logoImg.src =
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6UNjyD8lmAy-3Sg5Ode-qLW192ELE998TIg&usqp=CAU';
-    logoImg.alt = 'Musically Logo';
-    logoImg.width = '200px';
-  }, );
+
   const logoImg = document.createElement('img');
+  logoImg.src =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6UNjyD8lmAy-3Sg5Ode-qLW192ELE998TIg&usqp=CAU';
+  logoImg.alt = 'Musically Logo';
+  logoImg.width = '200px';
 
   const parrotDiv = document.createElement('div');
   parrotDiv.classList.add('parrot');
+
   startButton.appendChild(logoImg);
   startButton.appendChild(parrotDiv);
   startButtonContainer.appendChild(startButton);
 
-  // Append the elements to the phone image container
+  // Append elements to the phone image container
   itemDiv.appendChild(searchSongs);
   itemDiv.appendChild(searchButton);
   phoneImgContainer.appendChild(searchResults);
 
-  // Append the elements to the welcome container
+  // Append elements to the welcome container
   welcomeContainer.appendChild(welcomeHeader);
   welcomeContainer.appendChild(smallHeader);
   welcomeContainer.appendChild(phoneImgContainer);
@@ -89,10 +97,10 @@ function initializeWelcomePage() {
     // Simulate loading time
     setTimeout(function () {
       loadingContainer.style.display = 'none';
-      searchContainer.style.display = 'block'; 
+      searchContainer.style.display = 'block';
     }, 5000);
   });
-  
+
   // Add an event listener to the logo link
   const logoLink = document.getElementById('logo-link');
   logoLink.addEventListener('click', function (event) {
